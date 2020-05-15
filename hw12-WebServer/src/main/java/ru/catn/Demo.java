098b77d0afecf9234f8f07bd864733471d3885cf
Ekaterina <katrina_n@mail.ru>
2020-05-16 00:24:19 +0300

    Домашнее задание 12. Веб сервер

--- hw12-WebServer/src/main/java/ru/catn/Demo.java
+++ hw12-WebServer/src/main/java/ru/catn/Demo.java
@@ -1,13 +1,17 @@
 package ru.catn;
 
+import com.google.gson.ExclusionStrategy;
+import com.google.gson.FieldAttributes;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import org.hibernate.SessionFactory;
 import ru.catn.core.model.Address;
 import ru.catn.core.model.Phone;
 import ru.catn.core.model.User;
+import ru.catn.core.service.DBDataInitializer;
 import ru.catn.core.service.DBServiceUser;
 import ru.catn.core.service.DbServiceUserImpl;
+import ru.catn.hibernate.HibernateProxyTypeAdapter;
 import ru.catn.hibernate.HibernateUtils;
 import ru.catn.hibernate.dao.UserDaoHibernate;
 import ru.catn.hibernate.sessionmanager.SessionManagerHibernate;
@@ -17,9 +21,7 @@ import ru.catn.webserver.services.TemplateProcessor;
 import ru.catn.webserver.services.TemplateProcessorImpl;
 import ru.catn.webserver.services.UserAuthService;
 import ru.catn.webserver.services.UserAuthServiceImpl;
-
-import java.util.ArrayList;
-import java.util.List;
+import ru.catn.webserver.servlet.ExcludeJson;
 
 public class Demo {
     private static final int WEB_SERVER_PORT = 8080;
@@ -31,9 +33,25 @@ public class Demo {
         SessionManagerHibernate sessionManager = new SessionManagerHibernate(sessionFactory);
 
         DBServiceUser dbServiceUser = new DbServiceUserImpl(new UserDaoHibernate(sessionManager));
-        createUsers(dbServiceUser);
+        DBDataInitializer.createUsers(dbServiceUser);
 
-        Gson gson = new GsonBuilder().serializeNulls().setPrettyPrinting().create();
+        var excludeFieldsJson = new ExclusionStrategy() {
+            @Override
+            public boolean shouldSkipField(FieldAttributes fieldAttributes) {
+                return fieldAttributes.getAnnotation(ExcludeJson.class) != null;
+            }
+
+            @Override
+            public boolean shouldSkipClass(Class<?> aClass) {
+                return false;
+            }
+        };
+
+        Gson gson = new GsonBuilder().registerTypeAdapterFactory(HibernateProxyTypeAdapter.FACTORY)
+                .addSerializationExclusionStrategy(excludeFieldsJson)
+                .serializeNulls()
+                .setPrettyPrinting()
+                .create();
         TemplateProcessor templateProcessor = new TemplateProcessorImpl(TEMPLATES_DIR);
         UserAuthService authService = new UserAuthServiceImpl(dbServiceUser);
 
@@ -42,24 +60,5 @@ public class Demo {
 
         usersWebServer.start();
         usersWebServer.join();
-    }
-
-    public static void createUsers(DBServiceUser dbServiceUser) {
-        List<User> users = new ArrayList<>();
-
-        users.add(new User("Петров Пётр", "user1", "11111"));
-        users.add(new User("Иванов Иван", "user2", "11111"));
-        users.add(new User("Семенов Семен", "user3", "11111"));
-        users.add(new User("Ильяхов Илья", "user4", "11111"));
-        users.add(new User("Борисов Борис", "user5", "11111"));
-        users.add(new User("Сергеев Владимир", "user6", "11111"));
-        users.add(new User("Брэндон Смит", "user7", "11111"));
-
-        for (var user : users) {
-            user.setAddress(new Address("Симферопольский бульвар, 19"));
-            user.addPhone(new Phone("(495) 345 45 54"));
-            user.addPhone(new Phone("960 234 72 90"));
-        }
-        dbServiceUser.saveUsers(users);
     }
 }
--- hw12-WebServer/src/main/java/ru/catn/webserver/servlet/UsersApiServlet.java
+++ hw12-WebServer/src/main/java/ru/catn/webserver/servlet/UsersApiServlet.java
@@ -1,12 +1,8 @@
 package ru.catn.webserver.servlet;
 
-import com.google.gson.ExclusionStrategy;
-import com.google.gson.FieldAttributes;
 import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 import ru.catn.core.model.User;
 import ru.catn.core.service.DBServiceUser;
-import ru.catn.hibernate.HibernateProxyTypeAdapter;
 
 import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServlet;
@@ -30,26 +26,9 @@ public class UsersApiServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         User user = dbServiceUser.getUser(extractIdFromRequest(request)).orElse(null);
-
         response.setContentType("application/json;charset=UTF-8");
         ServletOutputStream out = response.getOutputStream();
-
-        var strategy = new ExclusionStrategy() {
-            @Override
-            public boolean shouldSkipField(FieldAttributes fieldAttributes) {
-                return fieldAttributes.getAnnotation(ExcludeJson.class) != null;
-            }
-
-            @Override
-            public boolean shouldSkipClass(Class<?> aClass) {
-                return false;
-            }
-        };
-        GsonBuilder gsonBuilder = new GsonBuilder();
-        out.print(gsonBuilder.registerTypeAdapterFactory(HibernateProxyTypeAdapter.FACTORY)
-                .addSerializationExclusionStrategy(strategy)
-                .create()
-                .toJson(user));
+        out.print(gson.toJson(user));
     }
 
     private long extractIdFromRequest(HttpServletRequest request) {
